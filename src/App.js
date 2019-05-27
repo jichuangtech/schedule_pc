ae253a2f65fbdc7051b34d75056b04309da76f07
huangbin <575395982@qq.com>
2019-05-28 03:47:56 +0800

    MOD 使用类的形式来创建Root根组建

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, message} from 'antd'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+    }
 
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <Button type="primary" onClick={() => {
+                        message.info("我是大黄蜂")
+                    }}>我是按钮</Button>
+                </header>
+            </div>);
+    }
+
+    componentDidMount() {
+        message.info(" 组建加载成功");
+    }
 }
 
 export default App;
